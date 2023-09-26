068a952982d0aa4bc69ea1b0cef49a3ab17054fa
nishinelson <naalnishinelson@gmail.com>
2023-09-26 12:50:11 -0700

    Draw divider

--- main.cpp
+++ main.cpp
@@ -13,7 +13,8 @@ int main () {
     while (WindowShouldClose() == false) {
         BeginDrawing();
 
-        //Drawing the ball and paddles
+        //Drawing the ball, paddles, and divider
+        DrawLine(screen_width/2, 0, screen_width/2, screen_height, WHITE);
         DrawCircle(screen_width/2, screen_height/2, 20, WHITE);
         DrawRectangle(10, screen_height/2 - 60, 25, 120, WHITE);
         DrawRectangle(screen_width - 35, screen_height/2 - 60, 25, 120, WHITE);
