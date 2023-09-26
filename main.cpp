6b60fa907f99b79be99b884c242edd6c461f1cbb
nishinelson <naalnishinelson@gmail.com>
2023-09-26 13:39:19 -0700

    Make the ball move

--- main.cpp
+++ main.cpp
@@ -3,6 +3,24 @@
 
 using namespace std;
 
+class Ball{
+    public:
+    float x, y;
+    int speed_x, speed_y;
+    int radius;
+
+    void Draw() {
+        DrawCircle(x, y, radius, WHITE);
+    }
+
+    void Update() {
+        x += speed_x;
+        y += speed_y;
+    }
+};
+
+Ball ball;
+
 int main () {
     cout << "starting the game" << endl;
     const int screen_width = 1280;
@@ -10,12 +28,22 @@ int main () {
     InitWindow(screen_width, screen_height, "My Pong Game!");
     SetTargetFPS(60);
 
+    ball.radius = 20;
+    ball.x = screen_width/2;
+    ball.y = screen_height/2;
+    ball.speed_x = 7;
+    ball.speed_y = 7;
+
     while (WindowShouldClose() == false) {
         BeginDrawing();
 
+        //Updating
+        ball.Update();
+
         //Drawing the ball, paddles, and divider
+        ClearBackground(BLACK);
         DrawLine(screen_width/2, 0, screen_width/2, screen_height, WHITE);
-        DrawCircle(screen_width/2, screen_height/2, 20, WHITE);
+        ball.Draw();
         DrawRectangle(10, screen_height/2 - 60, 25, 120, WHITE);
         DrawRectangle(screen_width - 35, screen_height/2 - 60, 25, 120, WHITE);
         EndDrawing();
